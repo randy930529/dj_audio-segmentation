8172d3eefb78a11b1367ee9daa19b98795696f42
Randy <randy930529@gmail.com>
2022-01-10 09:16:53 -0500

    Input audio file (wav, mp3, ogg)

--- frontend/src/components/AudioInputClip.js
+++ frontend/src/components/AudioInputClip.js
@@ -1,5 +1,5 @@
 export function AudioInputClip(props) {
     return (
-        <input className={props.class} type={props.type} name={props.name} onChange={props.onChange} />
+        <input className={props.class} type={props.type} name={props.name} accept="audio/wav, audio/mp3, audio/ogg" onChange={props.onChange} />
     )
 }
