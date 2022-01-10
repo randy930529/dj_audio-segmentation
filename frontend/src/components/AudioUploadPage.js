f9e73ae430d8e7bcf304d1994938e4a26f8b6e5a
Randy <randy930529@gmail.com>
2022-01-10 16:19:46 -0500

    upload audio clip file

--- frontend/src/components/AudioUploadPage.js
+++ frontend/src/components/AudioUploadPage.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { SingleTexto } from './SingleTexto';
-import { AudioInputClip } from './AudioInputClip';
 import { ButtonUpload } from './ButtonUpload';
 import { ButtonGo } from './ButtonGo';
 
 class AudioUploadPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [{
+                audioClip: null,
+            }],
+            isDisabled: false,
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.fileInput = React.createRef();
+    }
+
+    handleChange() {
+        const files = this.state.files.slice();
+        const file = files[files.length -1];
+        let audio = file.audioClip;
+        audio = this.fileInput.current.files[0];
+
+        this.setState({
+            files: files.concat([
+                {
+                    audioClip: audio
+                }
+            ]),
+            isDisabled:true,
+        });
+    }
 
 	render() {
+        const files = this.state.files;
+        const audio = files[files.length - 1];
+
         return(
         <div>
         <div className="notification is-primary m-3">
@@ -15,18 +44,23 @@ class AudioUploadPage extends React.Component{
                 value="Audio Input Clip" 
             />
 
-            <AudioInputClip 
-                class="input is-rounded" 
-                type="file" name="file" 
-                onChange={() => this.props.onChange()} 
+            <input 
+                className="input is-rounded" 
+                type="file"
+                name="file"
+                accept="audio/wav, audio/mp3, audio/ogg" 
+                ref={this.fileInput}
+                onChange={this.handleChange} 
             />
             
-            {!"isSelected" ?
+            {console.log(audio)}
+            {console.log(this.state.isDisabled)}
+            {this.state.isDisabled ?
                 (
                     <div>
                         <SingleTexto 
                             class="has-text-danger m-3" 
-                            value={"Filename: selectedAudio.name"} 
+                            value={`Filename: ${audio.audioClip.name}`} 
                         />
                     </div>
                 )
@@ -49,7 +83,7 @@ class AudioUploadPage extends React.Component{
                 <Link to={"/audio"}>
                     <ButtonGo 
                         class="button is-success is-rounded m-3" 
-                        disabled={!"isDisabled"?"disabled":""} 
+                        disabled={!this.state.isDisabled?"disabled":""} 
                         onClick={this.props.onClick} 
                         value="Go" 
                     />
