a9b57296511f15a76adbdcbc6ac8074e25ecfc9b
Randy <randy930529@gmail.com>
2022-02-10 12:50:58 -0500

    init config for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import styles from './App.module.css';
 import 'bulma';
-import LandigPage from './pages/LandigPage';
-import AudioCard from './pages/AudioCard';
+import AudioCardPage from './pages/AudioCardPage';
+import AudioUploadPage from './pages/AudioUploadPage';
+import {getContext} from "./utils/getUtilsAudioClip";
+
 
 function App() {
+  const context = getContext();
+  const [audioClip, setAudioClip] = useState();
+  const [file, setFile] = useState();
+  const [audioBuffer, setAudioBuffer] = useState();
+  const [position, setPosition] = useState(0);
+  const [showMarker, setShowMarker] = useState(false);
+
   return (
     <Router>
       <header>
@@ -13,8 +22,29 @@ function App() {
       </header>
       <main>
           <Routes>
-              <Route exact path="/audio" element={<AudioCard />} />
-              <Route path="/" element={<LandigPage />} />
+              <Route exact path="/audio/:audioClip" element={
+                <AudioCardPage 
+                  context={context} 
+                  file={file} 
+                  audioClip={audioClip} 
+                  buffer={audioBuffer} 
+                  setAudioBuffer={setAudioBuffer} 
+                  position={position}
+                  setPosition={setPosition}
+                  showMarker={showMarker}
+                  setShowMarker={setShowMarker}
+                />
+              } 
+              />
+              
+              <Route path="/" element={
+                <AudioUploadPage
+                  context={context} 
+                  setFile={setFile}
+                  setAudioClip={setAudioClip}
+                />
+              }
+              />
           </Routes>
       </main>
     </Router>
